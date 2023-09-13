acff33e9577cbdbdec65325d664f46515a2cf840
Famouus Bernard <famousbernard@gmail.com>
2023-09-13 04:56:17 -0700

    search algorithm

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,10 +11,12 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
+	size_t i;
+
 	if (array == NULL)
 		return (-1);
 
-	for (size_t i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		printf("Comparing %d\n", array[i]);
 		if (array[i] == value)
